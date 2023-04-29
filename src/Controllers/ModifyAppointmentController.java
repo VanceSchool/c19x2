215d0188e3590c770c183ff58ef8bf6071a11b11
LabUser <LabUser@DESKTOP-BT4131D>
2023-04-29 12:16:58 -0700

    Appointment Alert Created Works

--- src/Controllers/ModifyAppointmentController.java
+++ src/Controllers/ModifyAppointmentController.java
@@ -4,16 +4,22 @@
  */
 package Controllers;
 
+import static DAO.DAOAppointments.addAppointment;
+import static DAO.DAOAppointments.modifyAppointment;
 import static DAO.DAOContacts.findContactByID;
 import static DAO.DAOCustomers.findCustomeresByID;
+import static Helper.Alerts.alertGroupDatabaseChange;
 import static Helper.Alerts.alertGroupVerifyAction;
 import Helper.TimeMethods;
 import static Helper.TimeMethods.changeToEst;
+import static Helper.TimeMethods.changeUpLocaleDateTime;
 import Helper.UserfulMethods;
 import static Helper.UserfulMethods.*;
 import Models.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -77,13 +83,37 @@ public class ModifyAppointmentController implements Initializable {
     }    
 
     @FXML
-    private void handleAppointmentSavebt(ActionEvent event) {
+    private void handleAppointmentSavebt(ActionEvent event) throws IOException, SQLException {
         validateNonEmpty(descriptiontxt, locationtxt, typetxt, titletxt);
         validateHasSelection(AppointmentCustomercb,contactdd, startTimedd, endTimedd);
         validateHasDate(startDatepick);
         //validateHasTime(startTimedd.getValue(),endTimedd.getValue(),startDatepick.getValue());
         if(alertGroupVerifyAction(9)){
-            
+            Customers modAppointCust = AppointmentCustomercb.getValue();
+            Contacts modAppointCon = contactdd.getValue();
+            System.out.println(modAppointCon.getContactId());
+            System.out.println(modAppointCust.getCustomerID());
+            Appointments changeAppointment = new Appointments();
+            changeAppointment.setTitle(titletxt.getText());
+            changeAppointment.setDescription(descriptiontxt.getText());
+            changeAppointment.setLocation(locationtxt.getText());
+            changeAppointment.setType(typetxt.getText());
+            changeAppointment.setCustomerId(modAppointCust.getCustomerID());
+            changeAppointment.setContactId(modAppointCon.getContactId());
+            LocalDateTime starttime = changeUpLocaleDateTime(startDatepick.getValue(), startTimedd.getValue());
+            LocalDateTime endtime = changeUpLocaleDateTime(startDatepick.getValue(), endTimedd.getValue());
+            changeAppointment.setStart(Timestamp.valueOf(starttime));
+            changeAppointment.setEnd(Timestamp.valueOf(endtime));
+            changeAppointment.setAppointmentID();
+            modifyAppointment(changeAppointment);
+       
+            alertGroupDatabaseChange(3);
+            Parent root = FXMLLoader.load(getClass().getResource("/Scenes/Appointments.fxml"));
+            Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setTitle("Appointments");
+            stage.setScene(scene);
+            stage.show();   
         }return;
     }
 
@@ -95,11 +125,6 @@ public class ModifyAppointmentController implements Initializable {
         stage.setScene(scene);
         stage.show();
     }
-    public void setCurrentAppointment(Appointments appointment) {
-       
-    currentAppointment = appointment; 
-    appointmentModify(appointment); 
-    }
     
     public void appointmentModify(Appointments appointment){       
         currentAppointment = appointment;
@@ -108,7 +133,6 @@ public class ModifyAppointmentController implements Initializable {
         LocalTime startlt = startAppointment.toLocalTime();
         LocalTime endlt = endAppointment.toLocalTime();
         LocalDate appointDate = startAppointment.toLocalDate();
-        
         appointmentIDtxt.setText(String.valueOf(appointment.getAppointmentID()));
         titletxt.setText(currentAppointment.getTitle());
         descriptiontxt.setText(String.valueOf(currentAppointment.getDescription()));
@@ -155,16 +179,15 @@ public class ModifyAppointmentController implements Initializable {
 
     @FXML
     private void handleEndTimedd(ActionEvent event) {
-                if(startDatepick.getValue() != null){
-             LocalTime lt2 = endTimedd.getValue();
-             LocalDate ld = startDatepick.getValue();
-             LocalDateTime ldt2;
-                ldt2 = changeToEst(ld, lt2);
-             endTimelb.setText("Time EST " + ldt2.toLocalTime().toString());
+        if(startDatepick.getValue() != null){
+            LocalTime lt2 = endTimedd.getValue();
+            LocalDate ld = startDatepick.getValue();
+            LocalDateTime ldt2;
+            ldt2 = changeToEst(ld, lt2);
+            endTimelb.setText("Time EST " + ldt2.toLocalTime().toString());
         }else{
             endTimelb.setText("Please Choose Date");
         }
-        
     }
 
     @FXML
@@ -178,31 +201,31 @@ public class ModifyAppointmentController implements Initializable {
     }
     
     private void updateTimeText(){
-                        if((startTimedd.getValue() != null) && (endTimedd.getValue() != null)){
-             LocalTime lt1 = startTimedd.getValue();
-             LocalTime lt2 = endTimedd.getValue();
-             LocalDateTime ldt1;
-             LocalDateTime ldt2;
-             LocalDate ld = startDatepick.getValue();
-             ldt1 = changeToEst(ld, lt1);
-             startTimelb.setText("Time EST " + ldt1.toLocalTime().toString());
-              ldt2 = changeToEst(ld, lt2);
-             endTimelb.setText("Time EST " + ldt2.toLocalTime().toString());
-        } else if((startTimedd.getValue() != null) && (endTimedd.getValue() == null)){
-               LocalTime lt1 = endTimedd.getValue();
-             LocalDate ld = startDatepick.getValue();
-             LocalDateTime ldt1;
-              ldt1 = changeToEst(ld, lt1);
-             startTimelb.setText("Time EST " + ldt1.toLocalTime().toString());
-             endTimelb.setText("Please Choose Time");
+        if((startTimedd.getValue() != null) && (endTimedd.getValue() != null)){
+            LocalTime lt1 = startTimedd.getValue();
+            LocalTime lt2 = endTimedd.getValue();
+            LocalDateTime ldt1;
+            LocalDateTime ldt2;
+            LocalDate ld = startDatepick.getValue();
+            ldt1 = changeToEst(ld, lt1);
+            startTimelb.setText("Time EST " + ldt1.toLocalTime().toString());
+            ldt2 = changeToEst(ld, lt2);
+            endTimelb.setText("Time EST " + ldt2.toLocalTime().toString());
+        }else if((startTimedd.getValue() != null) && (endTimedd.getValue() == null)){
+            LocalTime lt1 = endTimedd.getValue();
+            LocalDate ld = startDatepick.getValue();
+            LocalDateTime ldt1;
+            ldt1 = changeToEst(ld, lt1);
+            startTimelb.setText("Time EST " + ldt1.toLocalTime().toString());
+            endTimelb.setText("Please Choose Time");
         } else if((startTimedd.getValue() == null) && (endTimedd.getValue() != null)){
-             LocalTime lt2 = endTimedd.getValue();
-             LocalDate ld = startDatepick.getValue();
-             LocalDateTime ldt2;
-              ldt2 = changeToEst(ld, lt2);
-             endTimelb.setText("Time EST " + ldt2.toLocalTime().toString());
-             startTimelb.setText("Please Choose Time");
-        }else {
+            LocalTime lt2 = endTimedd.getValue();
+            LocalDate ld = startDatepick.getValue();
+            LocalDateTime ldt2;
+            ldt2 = changeToEst(ld, lt2);
+            endTimelb.setText("Time EST " + ldt2.toLocalTime().toString());
+            startTimelb.setText("Please Choose Time");
+        }else{
             startTimelb.setText("Please Choose Time");
             endTimelb.setText("Please Choose Time");
         }
